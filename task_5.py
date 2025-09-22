00a2903e3df64b49911eebfb4d2d3058f079672c
yulia <tus10082006@gmail.com>
2025-09-22 03:08:49 +0300

    Add file

---
+++ task_5.py
@@ -0,0 +1,13 @@
+numbers = input("Введите число: ")
+print("Число:", numbers)
+n = int(numbers) #перевод строки в число
+if n % 7 == 0:
+    print("Магическое число!")
+else:
+    s = str(n) #перевод числа обратно в строку
+    a = int(s[0]) if len(s) > 0 else 0
+    b = int(s[1]) if len(s) > 1 else 0
+    c = int(s[2]) if len(s) > 2 else 0
+    d = int(s[3]) if len(s) > 3 else 0
+    e = int(s[4]) if len(s) > 4 else 0
+    print("Сумма цифр:", a + b + c + d + e)
