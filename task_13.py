f000c8c7b72d67741c25a659485b0031b8514fb4
yulia <tus10082006@gmail.com>
2025-09-30 10:31:10 +0300

    task_13.py без лишних функций

--- task_13.py
+++ task_13.py
@@ -1,24 +1,16 @@
 import random
+
 zagadannoe_chislo = random.randint(1, 100)
-print("Загаданно число от 1 до 100. Угадай.")
-# Счётчик попыток
-kolichestvo_popitok = 0
-# Бесконечный цикл
+print("Загадано число от 1 до 100. Угадай!")
+
 while True:
-    # Пользователь вводит число
     vvod = input("Введите число: ")
     chislo = int(vvod)
-    kolichestvo_popitok = kolichestvo_popitok + 1
-    print("Попытка номер", kolichestvo_popitok, ". Число:", chislo)
-    # Проверяем, угадал ли пользователь
-    if chislo == zagadannoe_chislo:
-        print("Угадано!")
-        print("Колличество попыток:", kolichestvo_popitok)
-        break  # выход из цикла
 
+    if chislo == zagadannoe_chislo:
+        print("Поздравляю! Ты угадал!")
+        break
     elif chislo < zagadannoe_chislo:
         print("Загаданное число больше.")
-
     else:
         print("Загаданное число меньше.")
-    print()
