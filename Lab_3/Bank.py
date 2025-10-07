9e4edd53bd33f5cc531a32de1efc9553f9f84cbc
yulia <tus10082006@gmail.com>
2025-10-07 14:34:51 +0300

    Bank.py 7.10

--- Lab_3/Bank.py
+++ Lab_3/Bank.py
@@ -18,9 +18,32 @@ class Client:
 
 
 
-
 class BankAccount:
     def __init__(self, client, volute):
         self.client = client
         self.volute = volute
         self.balance = 0.0
+
+
+
+def main():
+    bank = Bank
+
+    print("Приветствуем в нашем банке!")
+    name = input("Для регистрации введите ваше имя:")
+    client = Bank.register_account(name)
+    print(f"Поздравляем! Вы зарегистрированы. Ваш ID: {client.username_id}\n")
+    while True:
+        print("\n Доступные операции")
+        print("1. Открыть новый счет.")
+        print("2. Закрыть текущий счет.")
+        print("3. Пополнить текущий счет.")
+        print("4. Снять наличные.")
+        print("5. Перевод средств на другой счет.")
+        print("6. Получить выписку по текушему счету.")
+        print("0. Выйти.")
+
+        choice = input("Выберите операцию: ").strip()
+
+
+
