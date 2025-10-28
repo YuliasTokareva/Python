5841176259e66282003716505e4cf14040057053
yulia <tus10082006@gmail.com>
2025-10-28 14:00:44 +0300

    f-строки

--- task_7.py
+++ task_7.py
@@ -1,10 +1,10 @@
 # вводим количество секунд
 seconds_begin = int(input("Введите количество секунд: "))
-print("Вы ввели:", seconds_begin, "секунд")
+print(f"Вы ввели: {seconds_begin} секунд")
 minutes = seconds_begin // 60
-print("Целых минут:", minutes)
+print(f"Целых минут: {minutes}")
 ost_seconds = seconds_begin % 60
-print("Остаток секунд:", ost_seconds)
+print(f"Остаток секунд: {ost_seconds}")
 # Вывод
-print("--- РЕЗУЛЬТАТ ---")
-print(seconds_begin, "секунд — это", minutes, "минут и", ost_seconds, "секунд")
+print(f"--- РЕЗУЛЬТАТ ---")
+print(f"{seconds_begin} секунд — это {minutes} минут и {ost_seconds} секунд")
